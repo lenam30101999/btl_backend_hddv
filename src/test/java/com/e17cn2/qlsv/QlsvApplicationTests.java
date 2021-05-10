50494c23a32b20a810c39391f1be7f065196440c
phamphilongPTIT <phamphilong4101999@gmail.com>
2021-05-10 20:30:54 +0700

    create project qlsv

---
+++ src/test/java/com/e17cn2/qlsv/QlsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e17cn2.qlsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlsvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
